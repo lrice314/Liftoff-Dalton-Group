3d2a6b7ad1a1f514a11af48aee6945aadb07fddb
Lisa Rice <lisarice314@gmail.com>
2022-04-21 13:13:41 -0500

    hello world complete

---
+++ src/main/java/org/launchcode/liftoffdaltongroup/models/LiftoffDaltonGroupApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffdaltongroup.models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffDaltonGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffDaltonGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/liftoffdaltongroup/LiftoffDaltonGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffdaltongroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffDaltonGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
